ba2d89bc75e5fe2a02717c17f3048fc03c261c06
tdwhite0 <tdwhite0@gmail.com>
2015-04-25 11:32:36 -0400

    s

--- app.js
+++ app.js
@@ -154,7 +154,7 @@ app.router.get('/gpio/:cmd', function(cmd) {
 app.router.post('/hook', function(json) {
 
      this.res.writeHead(200, { 'Content-Type': 'text/plain' });
-  this.res.write(this.req);
+  this.res.write(this.req.body);
   this.res.end();
   console.log(this.req.body);
 });
